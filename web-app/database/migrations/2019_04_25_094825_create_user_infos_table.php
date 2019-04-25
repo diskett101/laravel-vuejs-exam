bedba6da084954ba0ad6c3fb598f267ddecd3271
melvinempleo <melvin.empleo.test@gmail.com>
2019-04-25 17:55:06 +0800

    Added UserInfo model and migration

---
+++ web-app/database/migrations/2019_04_25_094825_create_user_infos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_infos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('firstname', 50);
+            $table->string('lastname', 50);
+            $table->string('address');
+            $table->string('postcode', 8);
+            $table->string('phone_number', 10);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_infos');
+    }
+}
