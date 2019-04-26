38e6bc1eeb5910554ede7dba889f1f264c2e8df0
melvinempleo <melvin.empleo.test@gmail.com>
2019-04-26 17:01:14 +0800

    Updated controller

--- web-app/app/Http/Controllers/API/UserManagementController.php
+++ web-app/app/Http/Controllers/API/UserManagementController.php
@@ -85,8 +85,8 @@ class UserManagementController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(User $user)
     {
-        //
+        $user->delete();
     }
 }
