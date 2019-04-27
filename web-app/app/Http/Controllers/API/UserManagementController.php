dab3f2c9012d33a2f6cb988b3f74eb79f944d279
melvinempleo <melvin.empleo.test@gmail.com>
2019-04-27 12:49:51 +0800

    Removed unused controller methods

--- web-app/app/Http/Controllers/API/UserManagementController.php
+++ web-app/app/Http/Controllers/API/UserManagementController.php
@@ -30,15 +30,6 @@ class UserManagementController extends Controller
         return UserResource::collection($users);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -103,17 +94,6 @@ class UserManagementController extends Controller
     public function show(User $user)
     {
         return new UserResource($user);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
     }
 
     /**
